78de907fab45a597b2acb1b433d8b737a42f5bab
Arm Bun <bellotaophyc@outlook.com>
2022-09-04 17:37:52 +0000

    cleanup the extending files

--- yello.py
+++ yello.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env/ python
+
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
 
 @app.route('/profile/<username>')
 def profile(username):
     return "The Profile Section %s" % username
 
+manager = Manager(app)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    manager.run()
