fdbf282dde2318cbe9a04e6ee554159e64f42f6c
Arm Bun <bellotaophyc@outlook.com>
2022-09-08 17:48:27 +0000

    fix the SyntaxError in flaskBootstrap

--- yello.py
+++ yello.py
@@ -1,22 +1,11 @@
-#!/usr/bin/env/ python
-#from ensurepip import bootstrap
 
-#import os
-
-from flask_bootstrap import Bootstrap
 from flask import Flask 
 from flask import render_template
-from flask_moment import Moment
-from datetime import datetime
-#from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
-moment = Moment(app)
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/index')
 def index():
@@ -26,8 +15,5 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
-#manager = Manager(myapp)
-
 if __name__ == '__main__':
     app.run(debug=True)
-    #manager.run()
